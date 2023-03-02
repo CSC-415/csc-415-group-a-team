db6b92caa00b0a3afe17ea6c336280a58963f816
joeyblanchet <blanchetj2@mymail.nku.edu>
2023-03-02 12:29:04 -0500

    "temp canvas fragment"

--- app/src/main/java/com/example/canvasapp/UI/CanvasMainFragment.kt
+++ app/src/main/java/com/example/canvasapp/UI/CanvasMainFragment.kt
@@ -1,4 +1,21 @@
 package com.example.canvasapp.UI
 
-class CanvasMainFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.canvasapp.*
+
+class CanvasMainFragment : Fragment() {
+
+    //very basic but just inflates a blank view for now
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_canvas_view, container, false)
+    }
 }
