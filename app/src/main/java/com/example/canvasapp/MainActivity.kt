de001b3be505f2fb2a5078f4bfe472c257e51b83
Devyn Ferman <devynlferman@gmail.com>
2023-03-02 16:54:15 -0500

    Created the main menu view along with the fragment.

--- app/src/main/java/com/example/canvasapp/MainActivity.kt
+++ app/src/main/java/com/example/canvasapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.canvasapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.commit
-import com.example.canvasapp.UI.GalleryListFragment
+import com.example.canvasapp.UI.MainMenuFragment
 import com.example.canvasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
-            add(R.id.fragment_container_view, GalleryListFragment())
+            add(R.id.fragment_container_view, MainMenuFragment())
         }
     }
 }
