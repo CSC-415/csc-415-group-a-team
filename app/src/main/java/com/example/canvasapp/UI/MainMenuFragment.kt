4d5e2090cf2a5a75cf730fd44f00eb44f4649db0
joeyblanchet <blanchetj2@mymail.nku.edu>
2023-04-14 16:43:21 -0400

    Working Paint Function on a small canvas

--- app/src/main/java/com/example/canvasapp/UI/MainMenuFragment.kt
+++ app/src/main/java/com/example/canvasapp/UI/MainMenuFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.canvasapp.R
 import com.example.canvasapp.databinding.MainMenuViewBinding
+import com.example.canvasapp.ui.CanvasMainFragment
 
 class MainMenuFragment : Fragment() {
     private var _binding: MainMenuViewBinding? = null
@@ -17,6 +20,28 @@ class MainMenuFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = MainMenuViewBinding.inflate(inflater, container, false)
+
+        binding.NewCanvasButton.setOnClickListener {
+            requireActivity().supportFragmentManager.commit {
+                setReorderingAllowed(false)
+                replace(
+                    R.id.fragment_container_view,
+                    CanvasMainFragment()
+                )
+                addToBackStack(null)
+            }
+        }
+
+        binding.GalleryButton.setOnClickListener {
+            requireActivity().supportFragmentManager.commit {
+                setReorderingAllowed(false)
+                replace(
+                    R.id.fragment_container_view,
+                    GalleryListFragment()
+                )
+                addToBackStack(null)
+            }
+        }
         return binding.root
     }
 }
---
+++ app/src/main/java/com/example/canvasapp/UI/views/DrawView.kt
@@ -0,0 +1,76 @@
+package com.example.canvasapp.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import com.example.canvasapp.ui.CanvasMainFragment.Companion.paintBrush
+import com.example.canvasapp.ui.CanvasMainFragment.Companion.path
+
+
+class DrawView  : View {
+
+    var params : ViewGroup.LayoutParams? = null
+
+    companion object{
+        var pathList = ArrayList<Path>()
+        var colorList = ArrayList<Int>()
+        var currentBrush = Color.BLACK
+    }
+
+    constructor(context: Context) : this(context, null){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+        init()
+    }
+
+    private fun init() {
+        paintBrush.isAntiAlias = true
+        paintBrush.color = currentBrush
+        paintBrush.style = Paint.Style.STROKE
+        paintBrush.strokeJoin = Paint.Join.ROUND
+        paintBrush.strokeWidth = 8f
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        var x = event.x
+        var y = event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(x,y)
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(x,y)
+                pathList.add(path)
+                colorList.add(currentBrush)
+            }
+            else -> return false
+        }
+        //Use postInvalidate after changes on UI
+        postInvalidate()
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+
+        for(i in pathList.indices){
+            paintBrush.setColor(colorList[i])
+            canvas.drawPath(pathList[i], paintBrush)
+            invalidate() //changes done on UI
+        }
+
+    }
+}
